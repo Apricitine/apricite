52a57171be0080895267a8f0500d1f601079afa4
Apricitine <apricitinecas@gmail.com>
2022-08-20 13:35:32 -0700

    ⚒️Rewrite for Bug Fix⚒️

--- app.vue
+++ app.vue
@@ -1,29 +1,5 @@
 <template>
   <div>
-    <TopNavbar />
-    <div class="container">
-      <SideNavbar />
-    </div>
-    <main class="content">
-      <NuxtPage />
-    </main>
+    <NuxtWelcome />
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style lang="scss">
-@import "@/assets/sass/base.scss";
-@import "@/assets/sass/variables.scss";
-@import "@/assets/sass/mixins.scss";
-
-.container {
-  display: flex;
-}
-
-.content {
-  margin: 1rem;
-}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,6 @@
 import { defineNuxtConfig } from 'nuxt'
-import vue from '@vitejs/plugin-vue'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: [
-    "@/assets/sass/mixins.scss",
-    "@/assets/sass/variables.scss",
-  ],
-  head: {
-    title: "Apricitine",
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "@/assets/images/apricitine-logo.ico" },
-    ]
-  },
-  vite: {
-    plugins: [
-      vue({
-        reactivityTransform: true
-      })
-    ],
-  }
+
 })
