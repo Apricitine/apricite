f9ccb0bd656a92ddea56db0c6b013e97d70063b3
Apricitine <apricitinecas@gmail.com>
2022-08-27 19:13:34 -0700

    🖌️More Page Beautification🖌️

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -14,8 +14,10 @@
 
 nav.top-navbar {
   height: 10%;
+  min-height: $gui-min;
   background: $accent-dark1;
   position: fixed;
   width: 100%;
+  box-shadow: 0px 1px 10px $accent-dark1;
 }
 </style>
